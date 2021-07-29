5b8bdde608d4082c8d5f55fa8d1b29278da3761d
Joshua Howell <howjosh@gmail.com>
2021-07-28 21:20:30 -0500

    JH -- Write Time Difference -- Write time difference to excel file.

--- time_calculator.py
+++ time_calculator.py
@@ -1,5 +1,6 @@
 from datetime import datetime, date, time
 import re
+import openpyxl
 
 
 def convert_time_string(time_string):
@@ -12,4 +13,13 @@ def calculate_time_difference(start_time_string, end_time_string):
     return datetime.combine(date.min, end_time) - datetime.combine(date.min, start_time)
 
 
-print(calculate_time_difference("0:00:09,4", "0:00:10,7"))
+# print(calculate_time_difference("0:00:09,4", "0:00:10,7"))
+wb = openpyxl.load_workbook("Amanda.xlsx")
+
+sheet = wb["Sheet1"]
+start_time_string = sheet.cell(2,2).value
+end_time_string = sheet.cell(2,3).value
+time_difference = str(calculate_time_difference(start_time_string, end_time_string)).replace(".", ",", 1)
+
+sheet.cell(2, 4, value=time_difference)
+wb.save("Amanda.xlsx")
