8b03ab95872b3913aacb97012749a55d6465de40
Joshua Howell <howjosh@gmail.com>
2021-08-04 11:30:30 -0500

    JH -- Refactoring -- Renaming variables to make easier to read.

--- time_calculator_2.py
+++ time_calculator_2.py
@@ -10,26 +10,27 @@ max_number_of_rows = sheet.max_row
 row = 3
 
 while row <= 150:
-    # print("Inside while loop")
-    print(sheet.cell(row,4).value)
-    if(sheet.cell(row, 4).value):
-        # print("Starting search")
+    is_touching_expression = sheet.cell(row,4).value
+    # print(is_touching_expression)
+    if(is_touching_expression):
         start_row = row
-        end_row = row + 1
+        potential_end_row = row + 1
 
-        should_continue = True
+        should_continue_searching_for_end_row = True
 
-        while should_continue:
-            if(sheet.cell(end_row, 4).value):
-                end_row = end_row + 1
-                print("Keep incrementing end_row: ", end_row)
+        while should_continue_searching_for_end_row:
+            potential_end_row_is_touching_expression = sheet.cell(potential_end_row, 4).value
+
+            if(potential_end_row_is_touching_expression):
+                potential_end_row = potential_end_row + 1
+
             else:
-                print(end_row-1)
+                end_row = potential_end_row - 1
                 start_time = float(sheet.cell(start_row, 3).value)
-                end_time = float(sheet.cell(end_row - 1, 3).value)
+                end_time = float(sheet.cell(end_row, 3).value)
 
                 print(end_time - start_time)
-                should_continue = False
-                row = end_row
+                should_continue_searching_for_end_row = False
+                row = potential_end_row
     else:
         row = row + 1
