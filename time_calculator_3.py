adeb16bc58797b95b506bd0e071f3f06ae94df14
Joshua Howell <howjosh@gmail.com>
2021-08-04 15:02:08 -0500

    JH -- Element Touch Percentage -- Calculate percentage of time a user is touching an element in the expression.

--- time_calculator_3.py
+++ time_calculator_3.py
@@ -11,17 +11,17 @@ total_touch_time = 0
 print(max_number_of_rows)
 
 while row <= max_number_of_rows:
-    is_touching_expression = sheet.cell(row,4).value
-    if(is_touching_expression):
+    speech_string = sheet.cell(row,5).value
+    if(speech_string):
         start_row = row
         potential_end_row = row + 1
 
         should_continue_searching_for_end_row = True
 
         while should_continue_searching_for_end_row:
-            potential_end_row_is_touching_expression = sheet.cell(potential_end_row, 4).value
+            potential_end_row_is_touching_element = sheet.cell(potential_end_row, 5).value
 
-            if(potential_end_row_is_touching_expression):
+            if(potential_end_row_is_touching_element):
                 potential_end_row = potential_end_row + 1
 
             else:
@@ -30,7 +30,7 @@ while row <= max_number_of_rows:
                 end_time = float(sheet.cell(end_row, 3).value)
 
                 # print(f'Start row {start_row} -- End row {end_row}')
-                # print(end_time - start_time)
+                print(end_time - start_time)
                 total_touch_time = total_touch_time + (end_time - start_time)
                 should_continue_searching_for_end_row = False
                 row = potential_end_row
@@ -42,7 +42,7 @@ print(total_touch_time)
 total_time = sheet.cell(max_number_of_rows, 3).value - sheet.cell(3,3).value
 print(total_time)
 
-print(f'Total touch time {total_touch_time}')
+print(f'Total element touch time {total_touch_time}')
 print(f'Total time {total_time}')
 print(f'Percentage of time: {total_touch_time / total_time}')
 print(f'Percentage of time NOT touching: { 1 - total_touch_time / total_time}')
