5d8598bdab2f6269c1126d9b7c000a8e6ea824f8
Joshua Howell <howjosh@gmail.com>
2021-08-04 12:40:33 -0500

    JH -- Percentage touching expression -- Calculate percentage of time touching the expression.

--- time_calculator_2.py
+++ time_calculator_2.py
@@ -8,10 +8,12 @@ max_number_of_rows = sheet.max_row
 # sheet.cell(row, 1).value
 
 row = 3
+max_number_of_rows = sheet.max_row
+total_touch_time = 0
+print(max_number_of_rows)
 
-while row <= 150:
+while row <= max_number_of_rows:
     is_touching_expression = sheet.cell(row,4).value
-    # print(is_touching_expression)
     if(is_touching_expression):
         start_row = row
         potential_end_row = row + 1
@@ -29,8 +31,20 @@ while row <= 150:
                 start_time = float(sheet.cell(start_row, 3).value)
                 end_time = float(sheet.cell(end_row, 3).value)
 
-                print(end_time - start_time)
+                # print(f'Start row {start_row} -- End row {end_row}')
+                # print(end_time - start_time)
+                total_touch_time = total_touch_time + (end_time - start_time)
                 should_continue_searching_for_end_row = False
                 row = potential_end_row
     else:
         row = row + 1
+
+
+print(total_touch_time)
+total_time = sheet.cell(max_number_of_rows, 3).value - sheet.cell(3,3).value
+print(total_time)
+
+print(f'Total touch time {total_touch_time}')
+print(f'Total time {total_time}')
+print(f'Percentage of time: {total_touch_time / total_time}')
+print(f'Percentage of time NOT touching: { 1 - total_touch_time / total_time}')
